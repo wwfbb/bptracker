03d81c172d72e6e5596ee6f82b0f74a82058206a
Joel Singh <96002408+wwfbb@users.noreply.github.com>
2024-06-09 19:46:14 +0530

    Run npm format and type gymnastics

--- src/lib/data_fetch.ts
+++ src/lib/data_fetch.ts
@@ -18,10 +18,10 @@ export enum Time {
 }
 
 type TimedList = {
-  morning: Array<number>
-  afternoon: Array<number>
-  night: Array<number>
-}
+  morning: Array<number|null>;
+  afternoon: Array<number|null>;
+  night: Array<number|null>;
+};
 
 export async function fetchData(timeFrame: TimeFrame) {
   let duration;
@@ -40,9 +40,9 @@ export async function fetchData(timeFrame: TimeFrame) {
 
   let chart_labels: Array<string> = generateLabels(duration);
   let labels: Array<string> = [];
-  let hrValues: TimedList = {morning: [], afternoon: [], night: []};
-  let sysValues: TimedList = {morning: [], afternoon: [], night: []};
-  let diaValues: TimedList = {morning: [], afternoon: [], night: []};
+  let hrValues: TimedList = { morning: [], afternoon: [], night: [] };
+  let sysValues: TimedList = { morning: [], afternoon: [], night: [] };
+  let diaValues: TimedList = { morning: [], afternoon: [], night: [] };
 
   let { error, data } = await supabase
     .from("record")
@@ -74,10 +74,14 @@ export async function fetchData(timeFrame: TimeFrame) {
   const labels_deduped = [...new Set(labels)];
 
   for (const day of labels_deduped) {
-    const day_records = data.filter(record => { return DateTime.fromSQL(record.date).toFormat("LLL dd") == day })
+    const day_records = data.filter((record) => {
+      return DateTime.fromSQL(record.date).toFormat("LLL dd") == day;
+    });
 
-    const morning_record = day_records.filter(record => { return Number(record.time) == Time.MORNING })[0]
-    
+    const morning_record = day_records.filter((record) => {
+      return Number(record.time) == Time.MORNING;
+    })[0];
+
     if (morning_record) {
       hrValues.morning.push(morning_record.heart_rate);
       sysValues.morning.push(morning_record.value_sys);
@@ -88,8 +92,10 @@ export async function fetchData(timeFrame: TimeFrame) {
       diaValues.morning.push(null);
     }
 
-    const afternoon_record = day_records.filter(record => { return Number(record.time) == Time.AFTERNOON })[0]
-    
+    const afternoon_record = day_records.filter((record) => {
+      return Number(record.time) == Time.AFTERNOON;
+    })[0];
+
     if (afternoon_record) {
       hrValues.afternoon.push(afternoon_record.heart_rate);
       sysValues.afternoon.push(afternoon_record.value_sys);
@@ -100,7 +106,9 @@ export async function fetchData(timeFrame: TimeFrame) {
       diaValues.afternoon.push(null);
     }
 
-    const night_record = day_records.filter(record => { return Number(record.time) == Time.NIGHT })[0]
+    const night_record = day_records.filter((record) => {
+      return Number(record.time) == Time.NIGHT;
+    })[0];
 
     if (night_record) {
       hrValues.night.push(night_record.heart_rate);
@@ -112,10 +120,16 @@ export async function fetchData(timeFrame: TimeFrame) {
       diaValues.night.push(null);
     }
 
-    console.log(day, day_records)
+    console.log(day, day_records);
   }
 
-  return {  labels: labels_deduped, hrValues, sysValues, diaValues, chart_labels };
+  return {
+    labels: labels_deduped,
+    hrValues,
+    sysValues,
+    diaValues,
+    chart_labels,
+  };
 }
 
 export function getLabels(fetchedData) {
